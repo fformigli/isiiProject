1fcbdb616159dc2f324fefcb4ac3aa2eb76f1538
Fernando Formigli <formiglif@gmail.com>
2020-07-05 15:07:23 -0400

    preparando web

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
+//init
 const app = express();
 
+//settings
+app.set('port', process.env.PORT || 3000)
+
 //middlewares
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+//globals
 
 //routes
 app.use(require('./routes/index.js'));
 
-//routes
-app.listen(3000);
-console.log('Server running on port 3000');
+//public
+
+//start
+app.listen(app.get('port'), () => {
+    console.log('Server running on port '+app.get('port'));
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,12 @@ const { Router } = require('express');
 const router = Router();
 const { get_users, create_user , get_user_by_id, delete_user, update_user} = require('../controllers/index.controller');
 
+// web
+router.get('/', (req, res) => {
+    res.send('hello world :)');
+})
+
+//rest users
 router.get('/rest/users', get_users); //listar todo
 router.post('/rest/users', create_user); // crear
 router.get('/rest/users/:id', get_user_by_id); // listar un usuario
