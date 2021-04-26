e809871dc834d8fbeab80a3b92663582e60dee6a
fernando <fernando@mangoapp.com.py>
2021-04-26 18:28:19 -0400

    ajustes a task se agregaron los campos version, prioridad y observacion

---
+++ src/lib/constants.js
@@ -0,0 +1,14 @@
+TASK_STATUS_VALUES = [
+    { value: `iniciado`, label: `Iniciado` },
+    { value: `pendiente`, label: `Pendiente` },
+    { value: `finalizado`, label: `Finalizado` }
+]
+
+TASK_PRIORITY_VALUES = [
+    { value: 1, label: 'Urgente' },
+    { value: 2, label: 'Alta' },
+    { value: 3, label: 'Media' },
+    { value: 4, label: 'Baja' },
+]
+
+module.exports = { TASK_STATUS_VALUES, TASK_PRIORITY_VALUES }
